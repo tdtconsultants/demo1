3a0c8a0e15f2a5d3015529e776eae673f48ba508
Gabriel <gmadruga@tdtconsultants.com>
2020-02-12 15:33:40 -0300

    Fix

--- hr_appraisal_send_fix/__manifest__.py
+++ hr_appraisal_send_fix/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['web_enterprise'],
+    'depends': ['web_enterprise', 'hr_appraisal'],
 
     # always loaded
     'data': [
